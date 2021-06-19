620f1a8cea73ec87c8edfe9a9a12e3bd0d18ab5f
Arabi Alhumsi <araby.ami@gmail.com>
2021-06-19 15:55:05 +0200

    Add React Router

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
+
+function Routes () {
+    return (
+    <Router>
+        <Switch>
+            <Route exact path="/">
+                <h1>Main</h1>
+            </Route>
+        </Switch>
+    </Router>
+    )
+}
+
+export default Routes
+
